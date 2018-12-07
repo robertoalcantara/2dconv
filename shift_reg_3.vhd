973c36b0c7ad0835dbdff1e2c503039723029c34
Roberto Alcântara <roberto@eletronica.org>
2018-12-07 13:14:21 -0200

    Shift Register with 3 pixels output
    First commit.

---
+++ shift_reg_3.vhd
@@ -0,0 +1,93 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    20:08:45 12/06/2018 
+-- Design Name: 
+-- Module Name:    shift_reg_3 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity shift_reg_3 is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           input : in  STD_LOGIC_VECTOR (7 downto 0);
+           pixel1 : out  STD_LOGIC_VECTOR (7 downto 0);
+           pixel2 : out  STD_LOGIC_VECTOR (7 downto 0);
+           pixel3 : out  STD_LOGIC_VECTOR (7 downto 0));
+end shift_reg_3;
+
+architecture Behavioral of shift_reg_3 is
+
+	constant LINE_SIZE: integer:= 66; --imagem tem 64, com padding fica 66
+
+	type t_array is array (0 to LINE_SIZE-1) of std_logic_vector(7 downto 0);
+	
+	signal mem : t_array; -- 66x66, imagem 64x64 com padding
+	signal pixel1_next : std_logic_vector(7 downto 0);
+	signal pixel2_next : std_logic_vector(7 downto 0);
+	signal pixel3_next : std_logic_vector(7 downto 0);
+	signal input_next: std_logic_vector(7 downto 0);
+
+begin
+	
+--state and data registers
+process(clk,reset)
+begin
+  if reset='1' then
+		pixel1 <= (others=>'0');		
+		pixel2 <= (others=>'0');
+		pixel3 <= (others=>'0');
+		for cnt in 0 to LINE_SIZE-1 loop 
+			mem(cnt) <= "00000000";
+		end loop;
+		
+  elsif ( clk'event and clk='1') then
+	
+	for cnt in 0 to LINE_SIZE-2 loop 
+		mem(cnt) <= mem(cnt+1);
+	end loop;
+	mem(LINE_SIZE-1) <= input_next;
+    pixel1 <= pixel1_next;
+    pixel2 <= pixel2_next;
+    pixel3 <= pixel3_next;	
+  
+  end if;
+  
+end process;    
+
+
+--next-state logic and output logic
+process(input,mem(0),mem(1),mem(2))
+begin
+	input_next <= input;
+	pixel1_next <= mem(1);
+    pixel2_next <= mem(2);
+    pixel3_next <= mem(3);
+end process;
+
+
+
+end Behavioral;
+
---
+++ shift_reg_3_tb.vhd
@@ -0,0 +1,102 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   20:47:44 12/06/2018
+-- Design Name:   
+-- Module Name:   /home/robertoalcantara/Documents/Xilinx/Conv/shift_reg_3_tb.vhd
+-- Project Name:  Conv
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: shift_reg_3
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY shift_reg_3_tb IS
+END shift_reg_3_tb;
+ 
+ARCHITECTURE behavior OF shift_reg_3_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT shift_reg_3
+    PORT(
+         clk : IN  std_logic;
+         reset : IN  std_logic;
+         input : IN  std_logic_vector(7 downto 0);
+         pixel1 : OUT  std_logic_vector(7 downto 0);
+         pixel2 : OUT  std_logic_vector(7 downto 0);
+         pixel3 : OUT  std_logic_vector(7 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal reset : std_logic := '0';
+   signal input : std_logic_vector(7 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal pixel1 : std_logic_vector(7 downto 0);
+   signal pixel2 : std_logic_vector(7 downto 0);
+   signal pixel3 : std_logic_vector(7 downto 0);
+
+   -- Clock period definitions
+   constant clk_period : time := 1 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: shift_reg_3 PORT MAP (
+          clk => clk,
+          reset => reset,
+          input => input,
+          pixel1 => pixel1,
+          pixel2 => pixel2,
+          pixel3 => pixel3
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for 1 ns;
+		clk <= '1';
+		wait for 1 ns;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+   
+	  reset <= '1';
+	  wait for 2 ns;
+	  reset <= '0';
+   
+	  input <= "10101010";
+
+      wait;
+   end process;
+
+END;
